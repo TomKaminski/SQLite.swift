b858d892ce05920be13c6c0ff7e360b382c6ff69
Tomasz Kaminski <tomasz.kaminski@euvic.pl>
2022-12-19 18:20:17 +0100

    date format changewd

--- Sources/SQLite/Foundation.swift
+++ Sources/SQLite/Foundation.swift
@@ -63,9 +63,7 @@ extension Date: Value {
 /// custom `Value` type per additional format.
 public var dateFormatter: DateFormatter = {
     let formatter = DateFormatter()
-    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
-    formatter.locale = Locale(identifier: "en_US_POSIX")
-    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.ZZZZZ"
     return formatter
 }()
 
